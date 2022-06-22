22c9fd175f76b631eb8dedf1954343c51d9dc0a7
xiaomeng-zhu <xz2@wellesley.edu>
2022-06-22 18:47:55 +0000

    added comparison csv file

--- booknlp/chinese/model_test.py
+++ booknlp/chinese/model_test.py
@@ -3,94 +3,99 @@ from min_edit_distance import min_edit_distance
 from pos_match import parse_tok_pos
 from pos_match import pos_mismatch
 
-def convert_pos_jiagupos_pku(poss):
-    # takes in a list of pos
-    map_jiagupos_pku = { # a dictionary that maps jiagu pos tag to pku
-        "n":"n",
-        "nt":"t",
-        "nd":"f",
-        "nl":"s",
-        "nh":"nr",
-        "nhf":"nr",
-        "nhs":"nr",
-        "ns":"ns",
-        "nn":"n",
-        "ni":"nt",
-        "nz":"nz",
-        "v":"v",
-        "vd":"vd",
-        "vl":"v",
-        "vu":"v",
-        "a":"a",
-        "f":"b", #区别词
-        "mq":"m",
-        "m":"m",
-        "q":"q",
-        "d":"d",
-        "r":"r",
-        "p":"p",
-        "c":"c",
-        "u":"u",
-        "e":"e",
-        "o":"o",
-        "i": "i", # 习用语
-        "j":"j",
-        "h":"h",
-        "k":"k",
-        "g":"xxx", # in original pku tagset but not used by hanlp
-        "x":"x",
-        "w":"w",
-        "ws": "nx",
-        "wu":"w",
-    }
-    return [map_jiagupos_pku[pos] for pos in poss]
+convert_from = "pku" # by default, convert jiagu pos to pku
+
+map_jiagupos_pku = { # a dictionary that maps jiagu pos tag to pku
+    "n":"n",
+    "nt":"t",
+    "nd":"f",
+    "nl":"s",
+    "nh":"nr",
+    "nhf":"nr",
+    "nhs":"nr",
+    "ns":"ns",
+    "nn":"n",
+    "ni":"nt",
+    "nz":"nz",
+    "v":"v",
+    "vd":"vd",
+    "vl":"v",
+    "vu":"v",
+    "a":"a",
+    "f":"b", #区别词
+    "mq":"m",
+    "m":"m",
+    "q":"q",
+    "d":"d",
+    "r":"r",
+    "p":"p",
+    "c":"c",
+    "u":"u",
+    "e":"e",
+    "o":"o",
+    "i": "i", # 习用语
+    "j":"j",
+    "h":"h",
+    "k":"k",
+    "g":"xxx", # in original pku tagset but not used by hanlp
+    "x":"x",
+    "w":"w",
+    "ws": "nx",
+    "wu":"w",
+}
+
+map_pku_jiagupos = { # a dictionary that pku to jiagu pos
+    "Ag":"a",
+    "a":"a",
+    "ad":"d", # categorize 副形词 as 副词
+    "an":"n", # categorize 名形词 as 名词
+    "Bg":"b",
+    "b":"b",
+    "c":"c",
+    "Dg":"d",
+    "d":"d",
+    "e":"e",
+    "f":"nd",
+    "h":"h",
+    "i":"i",
+    "j":"j",
+    "k":"k",
+    "l":"i",
+    "Mg":"m",
+    "m":"m",
+    "Ng":"n",
+    "n":"n",
+    "nr":"nh",
+    "ns":"ns",
+    "nt":"ni",
+    "nx":"ws",
+    "nz":"nz",
+    "o":"o",
+    "p":"p",
+    "q":"q",
+    "Rg":"r",
+    "r":"r",
+    "s":"nl",
+    "Tg":"nt",
+    "t":"nt",
+    "u":"u",
+    "Vg":"v",
+    "v":"v",
+    "vd":"d", # categorize 副动词 as 副词
+    "vn":"n", # categorize 名动词 as 名词
+    "w":"w",
+    "x":"x",
+    "Yg":"u", # categorize 语气词 as 助词
+    "y":"u", # categorize 语气词 as 助词
+    "z":"a",
+}
+
+def convert_pos_jiagupos_pku(tok_poss):
+    # takes in a list of list of pos
+    return [(tok, map_jiagupos_pku[pos]) for tok, pos in tok_poss]
 
 def convert_pos_pku_jiagupos(tok_poss):
-    map_pku_jiagupos = { # a dictionary that pku to jiagu pos
-        "Ag":"a",
-        "a":"a",
-        "ad":"d", # categorize 副形词 as 副词
-        "an":"n", # categorize 名形词 as 名词
-        "Bg":"b",
-        "b":"b",
-        "c":"c",
-        "Dg":"d",
-        "d":"d",
-        "e":"e",
-        "f":"nd",
-        "h":"h",
-        "i":"i",
-        "j":"j",
-        "k":"k",
-        "l":"i",
-        "Mg":"m",
-        "m":"m",
-        "Ng":"n",
-        "n":"n",
-        "nr":"nh",
-        "ns":"ns",
-        "nt":"ni",
-        "nx":"ws",
-        "nz":"nz",
-        "o":"o",
-        "p":"p",
-        "q":"q",
-        "Rg":"r",
-        "r":"r",
-        "s":"nl",
-        "Tg":"nt",
-        "t":"nt",
-        "u":"u",
-        "Vg":"v",
-        "v":"v",
-        "vd":"d", # categorize 副动词 as 副词
-        "vn":"n", # categorize 名动词 as 名词
-        "w":"w",
-        "x":"x",
-        "Yg":"u", # categorize 语气词 as 助词
-        "y":"u", # categorize 语气词 as 助词
-        "z":"a",
-    }
+    # takes in a list of list of tok pos tuples
     return [(tok, map_pku_jiagupos[pos]) for tok, pos in tok_poss]
 
 """ input: list of sentences to tokenize
@@ -179,11 +184,13 @@ def jiagu_process_all(sentences, tok=True, pos=True, tokenized=[]):
         assert pos and tokenized != []
         all_pos = [jiagu.pos(toks) for toks in tokenized]
 
-        all_pos_converted = all_pos # comment this out if convert to pku for comparison
-        # all_pos_converted = list(map(convert_pos_jiagupos_pku, all_pos))
+        # if convert_from == "jiagu": # to pku
+        #     all_pos_converted = list(map(convert_pos_jiagupos_pku, all_pos))
+        # else:
+        #     all_pos_converted = all_pos # comment this out if convert to pku for comparison
         
         for i in range(len(tokenized)):
-            all_toks_pos.append(list(zip(tokenized[i], all_pos_converted[i])))
+            all_toks_pos.append(list(zip(tokenized[i], all_pos[i])))
         # print(all_toks_pos)
         return all_toks_pos
 
@@ -326,24 +333,34 @@ if __name__ == '__main__':
     standards_tok_pos = pd.read_csv("annotation/standard_pos.csv")
     standards_tok_pos = list(standards_tok_pos.iloc[:, 1])
     standards_tok_pos = parse_tok_pos(standards_tok_pos)
-    
-    standards_tok_pos_converted = list(map(convert_pos_pku_jiagupos, standards_tok_pos))
-    
+
     for model in model_list:
         res = process_tokenized_sents(sentences, model, standards_tok)
+        direction = ""
+    
         if model == "jiagu":
-            total_mismatch, average_mismatch, mismatch_list = pos_mismatch(standards_tok_pos_converted, res)
-        else:
-            total_mismatch, average_mismatch, mismatch_list = pos_mismatch(standards_tok_pos, res)
+            if convert_from == "jiagu": # convert jiagu pos to pku
+                res = list(map(convert_pos_jiagupos_pku, res))
+                direction = "_model2pku"
+            else: # convert gold standard to jiagu pos
+                standards_tok_pos = list(map(convert_pos_pku_jiagupos, standards_tok_pos))
+                direction = "_standard2jiagu"
+
+        total_mismatch, average_mismatch, mismatch_list = pos_mismatch(standards_tok_pos, res)
         
         print(model, total_mismatch, average_mismatch)
-        mismatch_df = pd.DataFrame(mismatch_list, columns=["sent_idx", "tok", "standard", model]).to_csv(model+"_pos_mismatch_tokens.csv")
+        file_name = "{}_pos_mismatch{}.csv".format(model, direction)
+        
+        
+        mismatch_df = pd.DataFrame(mismatch_list, 
+                columns=["sent_idx", "tok", "standard", model]).to_csv(file_name)
+        
 
     # average percent mismatch when convert jiagupos to pku
     # hanlp 253 0.16073418054397515
     # jiagu 428 0.2694341499643043
 
-    # average percent mismatch when convert pku to jiagupos
+    # average percent mismatch when convert gold standard pos in pku to jiagupos
     # hanlp 253 0.16073418054397515
     # jiagu 458 0.28878558958873424
 
@@ -381,7 +398,7 @@ if __name__ == '__main__':
     
 #     # output csv file for all tokenized results of all models
 #     df = pd.DataFrame(all_model_sents, index=model_list, columns=range(1,51))
-#     df.to_csv("model_results_50.csv")
+#     df.to_csv("model_results/model_results_50.csv")
         
 #     # for sentence in sentences:
 #     #     model_res = [] # list of results for each model
